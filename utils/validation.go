2ecca8512862854a8f547fad4ad31b8866430f09
j3ro3n <55191774+j3ro3n@users.noreply.github.com>
2021-05-28 14:06:11 +0200

    Update validation.go

--- utils/validation.go
+++ utils/validation.go
@@ -11,5 +11,6 @@ func IsOnion(identifier string) bool {
 		matched, _ := regexp.MatchString(`(^|\.)[a-z2-7]{16,56}\.onion$`, identifier)
 		return matched
 	}
+	fmt.Printf("identifier %s was skipped", identifier)
 	return false
 }
