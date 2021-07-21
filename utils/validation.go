faaf509529dc85f7f05bb2cefc33b9aa125ad37a
j3ro3n <55191774+j3ro3n@users.noreply.github.com>
2021-07-21 11:52:46 +0200

    Update validation.go
    Update validation to v3

--- utils/validation.go
+++ utils/validation.go
@@ -8,10 +8,10 @@ import (
 
 func IsOnion(identifier string) bool {
 	// TODO: At some point we will want to support i2p
-	if len(identifier) >= 22 && strings.HasSuffix(identifier, ".onion") {
-		isv2url, _ := regexp.MatchString(`(^|\.)[a-z2-7]{16}\.onion$`, identifier)
+	if len(identifier) >= 62 && strings.HasSuffix(identifier, ".onion") {
+		//isv2url, _ := regexp.MatchString(`(^|\.)[a-z2-7]{16}\.onion$`, identifier)
 		isv3url, _ := regexp.MatchString(`(^|\.)[a-z2-7]{56}\.onion$`, identifier)
-		return isv2url || isv3url
+		return isv3url // || isv2url
 	}
 	fmt.Printf("identifier %s was skipped", identifier)
 	return false
